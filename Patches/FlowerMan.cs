a7175d5a23dec9e72e819c98ac4d550b99f0b91c
Lill Q <96522235+ghotato@users.noreply.github.com>
2023-12-05 17:05:41 +0900

    Add files via upload

---
+++ Patches/FlowerMan.cs
@@ -0,0 +1,89 @@
+﻿using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlowerMorgan.Patches
+{
+  internal class FlowerMorganPatch
+  {
+    public class FlowermanAudioInfo
+    {
+      public bool IsPlaying { get; set; }
+      public AudioSource AudioSource { get; set; }
+    }
+
+    public static Dictionary<FlowermanAI, FlowermanAudioInfo> flowermanAudioInfoMap = new Dictionary<FlowermanAI, FlowermanAudioInfo>();
+
+    [HarmonyPatch(typeof(FlowermanAI), "Start")]
+    [HarmonyPostfix]
+    public static void FlowerMorganAudioPatch(FlowermanAI __instance)
+    {
+      if (!flowermanAudioInfoMap.TryGetValue(__instance, out var flowermanAudioInfo))
+        flowermanAudioInfo = new FlowermanAudioInfo
+        {
+          IsPlaying = false,
+          AudioSource = __instance.gameObject.AddComponent<AudioSource>()
+        };
+      flowermanAudioInfo.AudioSource.spatialBlend = 1f;
+      flowermanAudioInfo.AudioSource.dopplerLevel = 1f;
+      flowermanAudioInfo.AudioSource.rolloffMode = AudioRolloffMode.Linear;
+      flowermanAudioInfo.AudioSource.minDistance = 5f;
+      flowermanAudioInfo.AudioSource.maxDistance = 100f;
+      flowermanAudioInfo.AudioSource.volume = 1.0f;
+      flowermanAudioInfoMap[__instance] = flowermanAudioInfo;
+
+      __instance.crackNeckSFX = FlowerMorganModBase.audioClips[0];
+    }
+
+    [HarmonyPatch(typeof(FlowermanAI), nameof(FlowermanAI.Update))]
+    [HarmonyPrefix]
+    static void UpdatePatch(FlowermanAI __instance, ref bool ___wasInEvadeMode)
+    {
+      if (flowermanAudioInfoMap.TryGetValue(__instance, out var flowermanAudioInfo))
+      {
+        if (!___wasInEvadeMode && !flowermanAudioInfo.IsPlaying && Vector3.Distance(__instance.transform.position, __instance.GetClosestPlayer().serverPlayerPosition) <= 50f)
+        {
+          flowermanAudioInfo.IsPlaying = true;
+          FlowerMorganModBase.mls.LogWarning(__instance.GetInstanceID() + " Playing audio...");
+          flowermanAudioInfo.AudioSource.PlayOneShot(FlowerMorganModBase.audioClips[1]);
+          __instance.StartCoroutine(StopAudioAfterDelay(flowermanAudioInfo));
+        }
+      }
+    }
+    static IEnumerator StopAudioAfterDelay(FlowermanAudioInfo flowermanAudioInfo)
+    {
+      double randomDelay = new System.Random().NextDouble() * 5f + 30f;
+      yield return new WaitForSeconds(FlowerMorganModBase.audioClips[1].length + (float)randomDelay);
+      flowermanAudioInfo.AudioSource.Stop();
+      flowermanAudioInfo.IsPlaying = false;
+    }
+
+    [HarmonyPatch(typeof(FlowermanAI), nameof(FlowermanAI.HitEnemy))]
+    [HarmonyPrefix]
+    public static void FlowerMorganCleanPatch(FlowermanAI __instance)
+    {
+      if (__instance.enemyHP <= 0)
+      {
+        FlowerMorganModBase.mls.LogWarning(__instance.GetInstanceID() + " Killed!");
+        flowermanAudioInfoMap.Remove(__instance);
+      }
+    }
+
+    [HarmonyPatch(typeof(StartMatchLever), nameof(StartMatchLever.EndGame))]
+    [HarmonyPostfix]
+    public static void StartMatchLeverCleanUp()
+    {
+      FlowerMorganModBase.mls.LogWarning("Clearing `flowermanAudioInfoMap`...");
+      flowermanAudioInfoMap.Clear();
+    }
+
+    [HarmonyPatch(typeof(StartOfRound), "gameOverAnimation")]
+    [HarmonyPostfix]
+    public static void StartOfRoundCleanUp()
+    {
+      FlowerMorganModBase.mls.LogWarning("Clearing `flowermanAudioInfoMap`...");
+      flowermanAudioInfoMap.Clear();
+    }
+  }
+}
---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using FlowerMorgan.Patches;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace FlowerMorgan
+{
+  [BepInPlugin(modGUID, modName, modVersion)]
+  public class FlowerMorganModBase : BaseUnityPlugin
+  {
+    private const string modGUID = "Q.FlowerMorgan";
+    private const string modName = "Flower Morgan";
+    private const string modVersion = "1.0.0";
+
+    private readonly Harmony harmony = new Harmony(modGUID);
+    private static FlowerMorganModBase Instance;
+    public static ManualLogSource mls;
+
+    public static AudioClip[] audioClips = new AudioClip[2];
+
+    void Awake()
+    {
+      if (Instance == null)
+      {
+        Instance = this;
+      }
+
+      mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+      mls.LogInfo("Starting " + modName);
+      mls.LogWarning("Read sounds start!");
+      string[] paths = new string[]
+      {
+        "file://" + Paths.PluginPath + $@"\{modGUID}\rehehe.mp3",
+        "file://" + Paths.PluginPath + $@"\{modGUID}\smell.mp3"
+      };
+
+      for (int i = 0; i < paths.Length; i++)
+      {
+        mls.LogWarning("Sound: " + paths[i] + " Loadded!");
+        UnityWebRequest audioClip = UnityWebRequestMultimedia.GetAudioClip(paths[i], AudioType.MPEG);
+        audioClip.SendWebRequest();
+
+        while (!audioClip.isDone)
+        {
+        }
+        audioClips[i] = DownloadHandlerAudioClip.GetContent(audioClip);
+      }
+      mls.LogWarning("Read sounds end!");
+
+      harmony.PatchAll(typeof(FlowerMorganModBase));
+      harmony.PatchAll(typeof(FlowerMorganPatch));
+    }
+  }
+}
